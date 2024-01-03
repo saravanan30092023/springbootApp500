31865a7863052b958f8542b27ef3b0cc9bb0499a
Saravanan <saravanan.1.p@niit.com>
2024-01-03 05:12:51 +0000

    soln

---
+++ src/test/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springbootawsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert(true);
+	}
+
+}
