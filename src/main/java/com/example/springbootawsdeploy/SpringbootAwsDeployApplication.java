1b9f2efd1505e8807786024eeb115fd6b4972935
saravanan30092023 <146541365+saravanan30092023@users.noreply.github.com>
2024-01-04 13:21:59 +0530

    Update SpringbootAwsDeployApplication.java

--- src/main/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplication.java
+++ src/main/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootAwsDeployApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hai welcome to india ");
+		System.out.println("hai welcome to india niit ");
 		SpringApplication.run(SpringbootAwsDeployApplication.class, args);
 	}
 
