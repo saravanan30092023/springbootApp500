e7922b84a4f74b2a52264c342630979e9020e652
saravanan30092023 <146541365+saravanan30092023@users.noreply.github.com>
2024-01-04 11:45:15 +0530

    Update SpringbootAwsDeployApplication.java

--- src/main/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplication.java
+++ src/main/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootAwsDeployApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hai");
+		System.out.println("hai welcome");
 		SpringApplication.run(SpringbootAwsDeployApplication.class, args);
 	}
 
